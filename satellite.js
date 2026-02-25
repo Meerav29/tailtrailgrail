4bc799e0f99d52c6d043d8b0d53ade202aa71419
Meerav Shah <meeravshah29@gmail.com>
2026-02-24 22:30:42 -0500

    feat: add Sentinel-2 layer and enhance satellite layer management; introduce permissions configuration

--- satellite.js
+++ satellite.js
@@ -6,9 +6,17 @@ const SATELLITE_LAYERS = {
         attribution: '&copy; <a href="https://www.esri.com/">Esri</a>'
     },
     usgs: {
-        name: 'USGS Imagery',
-        url: 'https://basemap.nationalmap.gov/ArcGIS/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',
-        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">USGS</a>'
+        name: 'USGS Topo',
+        url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
+        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">USGS</a>',
+        maxNativeZoom: 16
+    },
+    sentinel: {
+        name: 'Sentinel-2 Cloudless',
+        url: 'https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/default/g/{z}/{y}/{x}.jpg',
+        attribution: '&copy; <a href="https://s2maps.eu">EOX IT Services GmbH</a>',
+        maxNativeZoom: 18,
+        updateWhenIdle: true
     }
 };
 
@@ -19,10 +27,21 @@ export function setSatelliteLayer(map, type) {
         map.satelliteLayer = null;
     }
 
+    // Show or hide the CartoDB street base layer
+    if (map._cartoBaseLayer) {
+        if (!type || type === 'none') {
+            if (!map.hasLayer(map._cartoBaseLayer)) map._cartoBaseLayer.addTo(map);
+        } else {
+            if (map.hasLayer(map._cartoBaseLayer)) map.removeLayer(map._cartoBaseLayer);
+        }
+    }
+
     if (!type || type === 'none' || !SATELLITE_LAYERS[type]) return;
 
     const info = SATELLITE_LAYERS[type];
-    map.satelliteLayer = L.tileLayer(info.url, { attribution: info.attribution });
+    const options = { attribution: info.attribution };
+    if (info.maxNativeZoom) options.maxNativeZoom = info.maxNativeZoom;
+    if (info.updateWhenIdle) options.updateWhenIdle = info.updateWhenIdle;
+    map.satelliteLayer = L.tileLayer(info.url, options);
     map.satelliteLayer.addTo(map);
-    map.satelliteLayer.bringToBack(); // ensure it stays behind labels if we had hybrid
 }
